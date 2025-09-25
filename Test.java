70695796a714fe680c4de829a01a206beba79ff8
Zona-firman <zonafirman2@gmail.com>
2025-09-25 08:23:48 +0700

    perubahan saya

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hi,ini baris Kode pertama saya");
+        System.out.println("Hi,ini adalah perubahan yang saya buat -27");
+
     }
 
 }
